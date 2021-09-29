9dc44e47dceb8ba5d4da4d67c03725d94cbe1af6
Saloni Jain <73168718+saloni070599@users.noreply.github.com>
2021-09-29 14:06:24 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,9 +1,9 @@
 /* HelloWorld.java
  */
 
-public class HelloWorld
+public class hello
 {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hello....i am here to run this code");
 	}
 }
